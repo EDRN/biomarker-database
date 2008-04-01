c128ab1a793bcfe5a5be7eeb0016e4a6a2348be9
Andrew Hart <afh@andrewfhart.com>
2008-04-01 23:45:11 +0000

    added study 'publications' view and actions

--- edit/study/actions.inc.php
+++ edit/study/actions.inc.php
@@ -1,3 +1,27 @@
 <?php
+	// Study::Associate Publication
+	if (isset($_POST['associate_publication'])) {
+		$sId = $_POST['study_id'];
+		$pId = $_POST['publication_id'];
+		try {
+			$s = StudyFactory::Retrieve($sId);
+			$s->link(StudyVars::PUBLICATIONS,$pId,PublicationVars::STUDIES);
+			XPressPage::httpRedirect("./?view=publications&id={$sId}");
+		} catch (XPressException $e) {
+			echo $e->getFormattedMessage();
+		}
+	}
 
+	// Study::Dissociate Publication
+	if (isset($_GET['remove_publication'])) {
+		$sId  = $_GET['id'];
+		$pId  = $_GET['remove_publication'];
+		try {
+			$s = StudyFactory::Retrieve($sId);
+			$s->unlink(StudyVars::PUBLICATIONS,$pId);
+			XPressPage::httpRedirect("./?view=publications&id={$sId}");
+		} catch (XPressException $e) {
+			echo $e->getFormattedMessage();
+		}
+	}
 ?>
