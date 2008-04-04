d98e17e59909cc4e9c039886ee6d03810692b285
Andrew Hart <afh@andrewfhart.com>
2008-04-04 03:08:02 +0000

    simplified the controller processing code added ajax_update which will process ajax-based update requests

--- edit/biomarker/index.php
+++ edit/biomarker/index.php
@@ -1,10 +1,9 @@
 <?php
 	require_once("../../xpress/app.php");
-	require_once("../../xpress/extensions/ajax_selectbox.php");
 	
 	// Determine the desired view
 	$view = (isset($_GET['view']))
-		? $_GET['view']
+		? strtolower($_GET['view'])
 		: 'basics';	// Default view is the 'basics' view
 
 	// Determine whether the page contents are editable
@@ -21,47 +20,48 @@
 	// Display the page
 	$p = new XPressPage("EDRN - Biomarker Database 0.3.0 Beta","text/html","UTF-8");
 	$p->includeCSS('../../static/css/frozen.css');
-	$p->includeJS('../../static/js/scriptaculous-js-1.7.0/lib/prototype.js');
-	$p->includeJS('../../static/js/scriptaculous-js-1.7.0/src/scriptaculous.js');
-	$p->includeJS('../../static/js/textInputs.js');
+	$p->includeCSS("../../static/css/eip.css");
+	$p->includeJS("../../static/js/mootools/mootools-release-1.11.js");
+	$p->includeJS("../../static/js/eip.js");
+	
+	$p->includeJS("view/{$view}.js");
 	$p->open();
-	// Load and display the menu
-	$p->view()->LoadTemplate('view/menu.html');
-	$p->view()->Show();
-	// Try to load and display the view
-	if ($p->view()->LoadTemplate("view/{$view}.html") ) {
-		// View-specific Processing
-		if ($view == "basics") {
-			$jsSecurity = generateAjaxSelectBoxJS('security',
-				$b->SecurityEnumValues,$b->_getType(),$b->getObjId(),BiomarkerVars::SECURITY,"../../xpress/js/AjaxHandler.php");
-			$jsQAState  = generateAjaxSelectBoxJS('qastate',
-				$b->QAStateEnumValues,$b->_getType(),$b->getObjId(),BiomarkerVars::QASTATE,"../../xpress/js/AjaxHandler.php");
-			$jsType     = generateAjaxSelectBoxJS('type',
-				$b->TypeEnumValues,$b->_getType(),$b->getObjId(),BiomarkerVars::TYPE,"../../xpress/js/AjaxHandler.php");
-		}
-		if ($view == "organs") {
-			$organDatas = $b->getOrganDatas();
-			foreach ($organDatas as $o) {$o->getOrgan();} // populate organ data
-			$p->view()->MergeBlock("organData",$organDatas);
-			$p->view()->MergeBlock("organs","mysql","SELECT objId,Name FROM Organ WHERE 1"); 
-		}
-		if ($view == "studies") {
-			$studyDatas = $b->getStudies();
-			foreach ($studyDatas as $s) {$s->getStudy();} // populate study data
-			$p->view()->MergeBlock("studyData",$studyDatas);
-			$p->view()->MergeBlock("studyDataJs",$studyDatas);
-		}
-		if ($view == "publications") {
-			$pubs = $b->getPublications();
-			$p->view()->MergeBlock("pub",$pubs);
-		}
-		if ($view == "resources") {
-			$resources = $b->getResources();
-			$p->view()->MergeBlock("res",$resources);
-		}
+	$p->view()->LoadTemplate("view/{$view}.html");
 
-		
-		$p->view()->Show();
+
+	// View-specific Processing
+	switch ($view) {
+		case "basics":
+			$securityOpts = str_replace(" ","_",implode("|",$b->SecurityEnumValues));
+			$qastateOpts  = str_replace(" ","_",implode("|",$b->QAStateEnumValues));
+			$typeOpts     = str_replace(" ","_",implode("|",$b->TypeEnumValues));
+			break;
+		case "organs":
+			$organDatas = $b->getOrganDatas();
+			foreach ($organDatas as $o) {
+				$o->getOrgan();			// Load organ data
+			}
+			$p->view()->MergeBlock("organData",$organDatas);
+			$p->view()->MergeBlock("organs","mysql","SELECT objId,Name FROM Organ"); 
+			break;
+		case "studies":
+			$studyDatas = $b->getStudies();
+			foreach ($studyDatas as $s) {
+				$s->getStudy();			// Load study data
+				$s->getPublications();	// Load referenced publications
+			}
+			$p->view()->MergeBlock("studyData",'array','studyDatas');
+			$p->view()->MergeBlock("studyPublications",'array','studyDatas[%p1%][Publications]');
+			break;
+		case "publications":
+			$p->view()->MergeBlock("pub",$b->getPublications());
+			break;
+		case "resources":
+			$p->view()->MergeBlock("res",$b->getResources());
+			break;
 	}
+
+	
+	$p->view()->Show();
 	$p->close();
 ?>
