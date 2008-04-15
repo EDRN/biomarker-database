ff8107892a17ee4cc63dc5b5e1f13a5560e38d10
Andrew Hart <afh@andrewfhart.com>
2008-04-15 22:54:25 +0000

    added ajax-enabled search box for biomarker browser

---
+++ browse/biomarkers/view/browse.js
@@ -0,0 +1,28 @@
+window.addEvent('domready',function() {
+
+  // Activate Study "Search" Autocomplete
+  new Autocompleter.Ajax.Xhtml(
+    $('biomarker-search'),
+    '../../xpress/extensions/ajax_autocomplete.php', {
+    'postData':{'object':'Biomarker','attr':'Title'},
+    'postVar': 'needle',
+    'target' : 'biomarker_id',
+    'parseChoices': function(el) {
+      if (el.getFirst()) {
+        $('biomarker-search').setStyle('background-color','#fff');
+        var value = el.getFirst().innerHTML;
+        var id    = el.getFirst().id;
+        el.inputValue = value;
+        el.inputId    = id;
+        this.addChoiceEvents(el).getFirst().setHTML(this.markQueryValue(value));
+      } else {
+        var value = '';
+        var id    = -1;
+        el.inputValue = value;
+        el.inputId    = id;
+        $(this.options.target).setProperty('value',el.inputId);
+        $('biomarker-search').setStyle('background-color','#fcc');
+      }
+   }
+   });
+});
