085bee27b12fec1b522cf21eb74052ab8da08f75
Andrew Hart <afh@andrewfhart.com>
2008-10-06 17:14:06 +0000

    model changes to support CA-98: Handling panels within the BMDB

--- app/models/biomarker.php
+++ app/models/biomarker.php
@@ -41,15 +41,72 @@ class Biomarker extends AppModel
 			'associationForeignKey'=>'publication_id',
 			'unique'=>true
 		),
+		/*
+		 * A 'Biomarker (Panel)' object HAS AND BELONGS TO MANY 'Biomarker' objects
+		 */
+		'Panel' => array(
+			'className' => 'Biomarker',
+			'join_table'=> 'biomarkers_biomarkers',
+			'foreignKey'=> 'panel_id',
+			'associationForeignKey'=>'biomarker_id',
+			'unique'=>true
+		)
 	
 	);
 	
+	public function clearPanelMembers($id) {
+		/* get rid of all membership records for this panel */
+		$q = "DELETE FROM `biomarkers_biomarkers` WHERE `panel_id` = '{$id}'";
+		$this->query($q);
+	}
+	
+	public function getAvailableBiomarkersForPanel($id) {
+		/* which biomarkers are NOT part of this panel */
+		$q = "SELECT `id` FROM `Biomarkers` WHERE `id` NOT IN "
+				."(SELECT `biomarker_id` FROM `biomarkers_biomarkers` "
+					."WHERE `panel_id`='{$id}') AND `id` <> '{$id}'";
+		
+		$results = $this->query($q);
+
+		$result = array();
+		foreach ($results as $r) {
+			$result[] = array("id"=>$r['Biomarkers']['id'],"name"=>$this->getDefaultNameById($r['Biomarkers']['id']));
+		}
+		return $result;
+	}
+	
+	public function getPanelMembership($id) {
+		/* which panels is this biomarker a member of? */
+		$q = "SELECT `panel_id` FROM `biomarkers_biomarkers` "
+			. "WHERE `biomarker_id`='{$id}'";	
+		$results = $this->query($q);
+		$result = array();
+		foreach ($results as $r) {
+			$result[] = array("id"=>$r['biomarkers_biomarkers']['panel_id'],"name"=>$this->getDefaultNameById($r['biomarkers_biomarkers']['panel_id']));
+		}
+		return $result;
+	}
+	
 	public static function getDefaultName($biomarker) {
+		/* what is the current default name (out of all the aliases) for this marker */
 		foreach ($biomarker['BiomarkerName'] as $name) {
 			if ($name['isPrimary'] == 1) {return $name['name'];}
 		}
 		return "unknown";
 	}
+	
+	public function getDefaultNameById($biomarker_id) {
+		/* same as above, only given a biomarker id */
+		$q = "SELECT `name` FROM `biomarker_names` WHERE `biomarker_id`='{$biomarker_id}' AND `isPrimary`='1'";
+		$results = $this->query($q);
+
+		if (count($results) > 0) {
+			return $results[0]['biomarker_names']['name'];
+		} else {
+			return "unknown";
+		}
+	}
+	
 	
 	var $actsAs = 'ExtendAssociations';
 	
