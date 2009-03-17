e2cb36abcf5a043cad5a61b4e2fa56c99890bf10
Andrew Hart <afh@andrewfhart.com>
2009-03-17 19:00:08 +0000

    Changes to support CA-258: biomarker-organ description value missing
    Include the free-text description of a BiomarkerOrgan in the 'biomarkerorgans' RDF export

--- app/controllers/rdf_controller.php
+++ app/controllers/rdf_controller.php
@@ -232,6 +232,7 @@ class RdfController extends AppController {
 			echo "  <bmdb:BiomarkerOrganData rdf:about=\"{$aboutURL}\">\r\n";
 			echo "    <bmdb:URN>urn:edrn:bmdb:biomarkerorgan:{$bod['OrganData']['id']}</bmdb:URN>\r\n";
 			echo "    <bmdb:Biomarker rdf:resource=\"http://{$this->getResourceBase()}/biomarkers/view/{$bod['Biomarker']['id']}\"/>\r\n";
+			echo "    <bmdb:Description>". $this->escapeEntities($bod['OrganData']['description']). "</bmdb:Description>\r\n";
 			echo "    <bmdb:Organ>{$bod['Organ']['name']}</bmdb:Organ>\r\n";
 			echo "    <bmdb:Phase>{$bod['OrganData']['phase']}</bmdb:Phase>\r\n";
 			echo "    <bmdb:QAState>{$bod['OrganData']['qastate']}</bmdb:QAState>\r\n";
