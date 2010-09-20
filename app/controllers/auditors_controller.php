908a956c2b23742b5fc2d0790750a3600615346a
Andrew Hart <afh@andrewfhart.com>
2010-09-20 21:21:00 +0000

    changes to resolve CA-653: password protect the BMDB audit trail
    The BMBD audit trail now calls checkSession() to force the user to authenticate
    first before being shown the contents of the BMDB audit trail.

--- app/controllers/auditors_controller.php
+++ app/controllers/auditors_controller.php
@@ -4,6 +4,7 @@ class AuditorsController extends AppController {
 	var $helpers = array('Html','Ajax','Javascript');
 	
 	public function weeklySummary() {
+		$this->checkSession("/auditors/weeklySummary");
 		$audits = $names = $this->Auditor->find("all",
 			array("order"=>'`timestamp` DESC'));
 		$this->set("audits",$audits);
