fb9ced9ab70111ad24233d0fa1d0a15732d5efd3
Andrew Hart <afh@andrewfhart.com>
2010-10-06 00:11:25 +0000

    progress towards CA-634/5: create basic model for 'Term' object

---
+++ app/models/term.php
@@ -0,0 +1,20 @@
+<?php
+class Term extends AppModel {
+	
+	
+	public $hasAndBelongsToMany = array(
+	
+		/*
+		 * A 'Term' object HAS AND BELONGS TO MANY 'OrganData' objects
+		 */
+		'OrganData' => array(
+			'className' => 'OrganData',
+			'join_table'=> 'organ_datas_terms',
+			'foreignKey'=> 'term_id',
+			'associationForeignKey' => 'organ_data_id'
+		),
+	);
+	
+	var $actsAs = 'ExtendAssociations';
+
+}
