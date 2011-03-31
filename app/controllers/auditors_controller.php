477ff7adcc9291a12a67e24fb00245e762a77bf0
Andrew Hart <afh@andrewfhart.com>
2011-03-31 22:47:35 +0000

    progress toward CA-753: provide statistical information on the content of the BMDB

--- app/controllers/auditors_controller.php
+++ app/controllers/auditors_controller.php
@@ -2,6 +2,25 @@
 class AuditorsController extends AppController {
 	
 	var $helpers = array('Html','Ajax','Javascript');
+	var $uses = 
+		array(
+			'Biomarker',
+			'BiomarkerName',
+			'Auditor',
+			'LdapUser',
+			'Organ',
+			'OrganData',
+			'Study',
+			'StudyData',
+			'BiomarkerStudyData',
+			'Publication',
+			'StudyDataResource',
+			'BiomarkerStudyDataResource',
+			'OrganDataResource',
+			'BiomarkerResource',
+			'Sensitivity',
+			'Term'
+		);
 	
 	public function weeklySummary() {
 		$this->checkSession("/auditors/weeklySummary");
@@ -9,6 +28,47 @@ class AuditorsController extends AppController {
 			array("order"=>'`timestamp` DESC'));
 		$this->set("audits",$audits);
 		
+	}
+	
+	public function metrics() {
+		$this->checkSession("/auditors/metrics");
+		
+		/***
+		 * BIOMARKER METRICS
+		 *************************************************************/
+		$biomarkers = $this->Biomarker->find('all',array(
+			'conditions' => array(),
+			'recursive'  => 1,
+			"order"=>'`id` DESC'
+		));
+		
+		// Populate each biomarker with the names of its default name
+		for ($i=0;$i<count($biomarkers);$i++) {
+			$biomarkers[$i]['DefaultName']= $this->Biomarker->getDefaultName($biomarkers[$i]);
+		}
+		
+		// Send it off to the view
+		$this->set('numBiomarkers',count($biomarkers));
+		$this->set('latestBiomarkers', array_splice($biomarkers,0,5));
+		
+		
+		/***
+		 * PROTOCOL METRICS
+		 *************************************************************/
+		$studies = $this->Study->find("all",array(
+			'conditions' => array(),
+			'recursive'  => 1,
+			'order'=>'`id` DESC'
+		));
+		$this->set('numProtocols',count($studies));
+		$this->set('latestProtocols',array_splice($studies,0,5));
+		
+		/***
+		 * PUBLICATION METRICS
+		 *************************************************************/
+		$pubs = $this->Publication->findAll();
+		$this->set('numPublications',count($pubs));
+		$this->set('latestPublications', array_splice($pubs,0,5));
 	}
 }
 ?>
